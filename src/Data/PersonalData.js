f496fe80e22b726f00d880e138a0a584d22a5222
memo001x <mohamedibrahimhennani@gmail.com>
2024-11-26 02:56:14 +0100

    Add files via upload

---
+++ src/Data/PersonalData.js
@@ -0,0 +1,11 @@
+const PersonalData = {
+    firstName: "Abdelhadi ",
+    lastName: "El Atifi",
+    mobNo:"+212 8299069282",
+    email:"abdelhadi2@gmail.com",
+    address:"Morocco, Tinghir, Ait Mohamemed",
+    aboutMe:"Hi Everyone, I am Abdelhadi El Atifi from Tinghir City, Morocco.I learned C, C++, Python, JavaScript etc, Worked with libraries & frameworks like ReactJS, NodeJS. Have a keen knowledge of databases like MySQL, MongoDB and Firebase. Eager to contribute to innovative projects and gain hands-on experience in a dynamic and collaborative",
+    imageUrl:''
+}
+export const autoTypeData=['Bac +2 IT Grad', 'Full Stack Developer','Football Pro Player in Ait Mohammed', 'Open Source Contributor'];
+export default PersonalData;
---
+++ src/Data/SocialData.js
@@ -0,0 +1,16 @@
+const githubLink="https://github.com/";
+const instaLink="https://instagram.com/";
+const linkedInLink="https://www.linkedin.com/";
+const twitterLink="https://twitter.com/";
+const emailLink="gmail.com";
+
+const profileLinks={
+    githubLink,
+    instaLink,
+    linkedInLink,
+    twitterLink,
+    emailLink
+}
+const SocialData=profileLinks;
+export default SocialData;
+
